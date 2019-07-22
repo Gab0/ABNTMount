8cbf8037a5667bd011f111cd03602f85ab42d12e
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-07-22 13:01:30 -0300

    v0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='ABNTMount',
     author="Gab0",
-    version='0.4',
+    version='0.5',
     packages=['ABNTMount'],
     license='GPLv2',
     long_description=open('README.md').read(),
