990934c5289f2e6efe5da7bc5e13f68ee340628d
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-07-22 13:01:27 -0300

    customizable document replacement tokens

--- ABNTMount/texFileParser.py
+++ ABNTMount/texFileParser.py
@@ -1,38 +1,28 @@
 #!/bin/python
 
+
 def parseTexFile(texFilePath,
-                 PretextualSequence,
-                 ManuscriptSequence,
+                 segmentSequences,
+                 replacementTokens,
                  ManuscriptParts):
 
     texFile = open(texFilePath).read()
-    pretextualReplacementToken = "%[[PRETEXTUAL-CONTENT]]"
-    chapterReplacementToken = "%[[CHAPTER-CONTENT]]"
 
-
-    pretextualLines = []
-    if pretextualReplacementToken in texFile:
-        for p, Pretextual in enumerate(PretextualSequence):
-            pretextualdef = "\input{%s}" % Pretextual
-            pretextualLines.append(pretextualdef)
-
-
-    texFile = texFile.replace(pretextualReplacementToken,
-                              "\n".join(pretextualLines))
-    chapterLines = []
-    if chapterReplacementToken in texFile:
-        for m, Manuscript in enumerate(ManuscriptSequence):
-            for PartIndex, PartName in ManuscriptParts:
-                if PartIndex == m:
-                    partdef = "\part{%s}" % PartName
-                    chapterLines.append(partdef)
-                    break
-
-            Line = "\input{%s}" % Manuscript
-            chapterLines.append(Line)
-
-    texFile = texFile.replace(chapterReplacementToken,
-                              "\n".join(chapterLines))
+    for s, segmentSequence in enumerate(segmentSequences):
+        replacementToken = replacementTokens[s]
+        segmentLines = []
+        if replacementToken in texFile:
+            for m, SegmentFilepath in enumerate(segmentSequence):
+                if ManuscriptParts[s]:
+                    for PartIndex, PartName in ManuscriptParts[s]:
+                        if PartIndex == m:
+                            partdef = "\part{%s}" % PartName
+                            segmentLines.append(partdef)
+                            break
+                segmentBlock = "\input{%s}" % SegmentFilepath
+                segmentLines.append(segmentBlock)
+                segmentLines.append("\clearpage")
+        texFile = texFile.replace(replacementToken,
+                                  "\n".join(segmentLines))
 
     return texFile
-
